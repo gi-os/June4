6f986c82adec9299c0f235052e48db152f0e3a3f
Giovanni Lupo <gio@gi-os.com>
2017-02-28 19:33:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 var Discordie = require('discordie');
 const Events = Discordie.Events;
 const client = new Discordie();
+
 client.connect({
-token: 'Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ'
+    token: 'Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ'
 });
+
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
-console.log('Connected as ' + client.User.username);
+    console.log('Connected as ' + client.User.username);
 });
-if(e.message.content == 'PING') {
-e.message.channel.sendMessage('PONG')
-}
+
+if (e.message.content == 'PING') {
+    e.message.channel.sendMessage('PONG')
 });
